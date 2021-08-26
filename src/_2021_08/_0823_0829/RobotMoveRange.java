b38939bc218abce6cc5abc795bec2b69f4cc5737
jiayongqiang <jiayongqiang@xiaomi.com>
2021-08-26 22:26:12 +0800

    [0826]
    1. Robot move range (BFS)

--- src/_2021_08/_0823_0829/RobotMoveRange.java
+++ src/_2021_08/_0823_0829/RobotMoveRange.java
@@ -1,5 +1,8 @@
 package _2021_08._0823_0829;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * [题目]：剑指 Offer 13. 机器人的运动范围
  * 
@@ -10,11 +13,43 @@ package _2021_08._0823_0829;
  * 
  * [原题链接]：https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof
  * 
- * [解决方案]：
+ * [解决方案]：方法一：广度优先遍历
  */
 public class RobotMoveRange {
 
     public int movingCount(int m, int n, int k) {
+        // 特殊判断
+        if (k == 0) return 1;
+        Deque<int[]> deque = new LinkedList<>();
+        int count = 1;
+        // x、y轴方向值，放在数组中便于计算
+        int[] dx = {0, 1};
+        int[] dy = {1, 0};
+        // 标记是否为访问过的元素
+        boolean[][] visited = new boolean[m][n];
+        deque.offer(new int[]{0, 0});
+        while (!deque.isEmpty()) {
+            int[] cell = deque.poll();
+            for (int i = 0; i < 2; i++) {
+                int x = cell[0] + dx[i];
+                int y = cell[1] + dy[i];
+                if (x >= m || y >= n || visited[x][y] || getSum(x) + getSum(y) > k) {
+                    continue;
+                }
+                visited[x][y] = true;
+                deque.offer(new int[]{x, y});
+                count++;
+            }
+        }
+        return count;
+    }
 
+    private int getSum(int i) {
+        int ans = 0;
+        while (i != 0) {
+            ans += i % 10;
+            i /= 10;
+        }
+        return ans;
     }
 }
